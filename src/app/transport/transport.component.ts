f3ef8930b11c04aea3a1fd244027be786382566a
Louis Stockreisser <louis.stockreisser@gmail.com>
2016-10-10 13:41:17 +0200

    [LST] Change search results layout (give up tables) + fire searches on date tabs

--- src/app/transport/transport.component.ts
+++ src/app/transport/transport.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { FormGroup, FormControl, Validators, FormBuilder }  from '@angular/forms';
+import * as spinner from 'ng2-spin-kit/app/spinners';
 
 import { TransportService } from '../services/transport.service';
 
 @Component({
   selector: 'app-transport',
   templateUrl: './transport.component.html',
-  styleUrls: ['./transport.component.css']
+  styleUrls: ['./transport.component.css'],
+  directives: [
+    spinner.RotatingPlaneComponent,
+    spinner.DoubleBounceComponent,
+    spinner.WaveComponent,
+    spinner.WanderingCubesComponent,
+    spinner.PulseComponent,
+    spinner.ChasingDotsComponent,
+    spinner.CircleComponent,
+    spinner.ThreeBounceComponent,
+    spinner.CubeGridComponent,
+    spinner.WordPressComponent,
+    spinner.FadingCircleComponent,
+    spinner.FoldingCubeComponent
+  ]
 })
 export class TransportComponent implements OnInit {
 
@@ -23,6 +38,8 @@ export class TransportComponent implements OnInit {
   private results = {};
   private flights = [];
   private isLoading = true;
+  private init = false;
+  private searching = false;
 
   private flight = {};
   private isEditing = false;
@@ -41,9 +58,8 @@ export class TransportComponent implements OnInit {
               private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.getFlights(this.query);
-    this.browseFlights(this.query);
-    console.log(this.flights);
+    this.init = false;
+    this.isLoading = false;
   }
 
   getFlights(query) {
@@ -51,15 +67,39 @@ export class TransportComponent implements OnInit {
     this.transportService.getFlights(this.query).subscribe(
       data => this.results = data,
       error => console.log(error),
-      () => this.isLoading = false
+      () => {
+        console.log("after getFlights")
+        console.log("found "+this.results.flights.length+" flights")
+        this.isLoading = false;
+        this.searching = false;
+      }
     );
   }
   browseFlights(query) {
     this.transportService.browseFlights(this.query).subscribe(
       data => this.browseResults = data,
-      error => console.log(error)
+      error => console.log(error),
+      () => {
+        console.log("after browseFlights")
+        console.log("found "+this.browseResults.length+" browses")
+      }
     );
     console.log(this.flights)
+  }
+  search(){
+    this.init = true;
+    console.log("calling search")
+    console.log(this.query)
+    this.searching = true;
+    this.getFlights(this.query);
+    this.browseFlights(this.query);
+  }
+  switchDate(newDate){
+    console.log("switching dates")
+    this.query.singleDate = newDate;
+    console.log(this.query)
+    this.getFlights(this.query);
+    this.browseFlights(this.query);
   }
 
 }
